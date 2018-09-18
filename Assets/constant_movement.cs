eff83f4b7b2df136bacc91ffbfa254a77acc5850
Vishal Shelat <vishalshelat@gmail.com>
2018-09-18 14:17:37 -0700

    added unity project files

---
+++ Assets/constant_movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class constant_movement : MonoBehaviour {
+public Rigidbody2D man_move;
+public float screenspeed;
+public void FixedUpdate(){
+     man_move.velocity = new Vector2(screenspeed,man_move.velocity.y);
+}
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/mc_movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mc_movement : MonoBehaviour {
+
+public Rigidbody2D man_move;
+public float jumppower;
+
+private void Update(){
+    bool jump = Input.GetButtonDown("Jump");
+    if (jump){
+        man_move.AddForce(new Vector2(0f,jumppower),ForceMode2D.Impulse);
+    }
+        
+}
+}
