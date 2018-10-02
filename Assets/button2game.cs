4042662f3e9d4cd9d652b62472ae1592ed3a39dd
Vishal Shelat <vishalshelat@gmail.com>
2018-10-02 14:50:55 -0700

    moved button to game file

---
+++ Assets/button2game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class button2game : MonoBehaviour {
+    
+    public void loadlevel(int levelindex){
+        SceneManager.LoadScene(levelindex);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
