0a5f1366630edd8a7dfe961a7e4c8e6f85f487ca
Vishal Shelat <vishalshelat@gmail.com>
2018-10-09 01:17:56 -0700

    added sound

---
+++ Assets/enemy scripts/girlscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class girlscript : MonoBehaviour {
+
+	public void OnCollisionEnter2D(Collision2D onhit){
+        if (onhit.collider.gameObject.name == "MC"){          
+            Destroy(onhit.collider.gameObject);
+
+        }
+ 
+    }
+    
+	void Update () {
+		
+	}
+}
